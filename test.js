ed20af24acb3248070d81dd7cd41af7888dfdf53
phakphum-debug <phakphum.sic68@prtc.ac.th>
2026-05-21 13:52:21 +0700

    Day-1 learning create variable and operator

---
+++ test.js
@@ -0,0 +1,4 @@
+console.log("js-IT2")
+console.log("test console log")
+var a //variable
+a=10
---
+++ variables.js
@@ -0,0 +1,17 @@
+var text ="gories";
+var num = 35;
+var point = 3.14;
+console.log("text,num,point");
+var a =10, b =12, c =15
+console.log("a,b,c");
+
+
+let x =123
+let y
+
+
+
+
+
+
+=123
